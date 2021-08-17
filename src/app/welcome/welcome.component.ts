e27f5e7038f6d1b6b6cac3fd6fe356a6c7425f87
Leo Lanese <developer@leolanese.com>
2021-08-17 21:27:22 +0100

    improvements

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService }       from './user.service';
+
+@Component({
+  selector: 'app-welcome',
+  template: '<h3 class="welcome"><i>{{welcome}}</i></h3>'
+})
+export class WelcomeComponent  implements OnInit {
+  welcome: string|undefined;
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.welcome = this.userService.isLoggedIn ?
+      'Welcome, ' + this.userService.user.name : 'Please log in.';
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
